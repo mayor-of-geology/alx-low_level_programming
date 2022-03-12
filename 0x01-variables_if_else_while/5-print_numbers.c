a055876204307bfb6f3aa0a132d6e4a38ed99ebe
Joshua Atolagbe <atolagbejoshua2@gmail.com>
2022-03-12 08:10:30 -0800

    [Update:]New additional files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+ * main - Prints numbers from 0 to 9 in base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
