b5ab8dba8910682b934fa68cb6bc86fe452ea8ec
Joshua Atolagbe <99275929+mayor-of-geology@users.noreply.github.com>
2022-03-16 15:40:45 +0100

    [Update;] adjusted the script for betty doc style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_sign -- prints the sign of a number 
+ *print_sign - prints the sign of a number 
  * @n: The input integer number
  * Return: 1 if number is greater than zero
  * Return: 0 if number is is zero
- * Return: -1 if number is less than zero 
+ *Return: -1 if number is less than zero 
  */
 int print_sign(int n)
 {
