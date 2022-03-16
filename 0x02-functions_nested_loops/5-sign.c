74a74ff56c9e9a7b2bfdaac15e399fbc251c2210
Joshua Atolagbe <99275929+mayor-of-geology@users.noreply.github.com>
2022-03-16 15:34:24 +0100

    [Update;} adjusted the script for betty doc style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,11 +5,11 @@
  * 
  * @n: The input integer number
  *
- * Return 1 if number is greater than zero
+ * Return: 1 if number is greater than zero
  * 
- * Return 0 if number is is zero
+ * Return: 0 if number is is zero
  * 
- * Return -1 if number is less than zero 
+ * Return: -1 if number is less than zero 
  */
 int print_sign(int n)
 {
