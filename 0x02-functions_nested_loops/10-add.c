581a00615cca4858c08988d5dd827548dc566893
Joshua Atolagbe <99275929+mayor-of-geology@users.noreply.github.com>
2022-03-16 16:08:12 +0100

    [Update;} adjusted the script for betty doc style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,9 @@
 
 /**
  * add -- adds two integers and returns the result
- *
+ * @a: first integer argument
+ * @b: second integer argument
+ * Return: the addition of two integer values
  */
 int add(int a, int b)
 {
