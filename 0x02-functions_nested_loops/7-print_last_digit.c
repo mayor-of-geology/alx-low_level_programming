49bc7e85d35e01e1c4b9d201a48c0e99ff91d311
Joshua Atolagbe <99275929+mayor-of-geology@users.noreply.github.com>
2022-03-16 15:52:20 +0100

    [Update;} adjusted the script for compilation

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,9 +5,9 @@
  *
  * @n -- input integer
  *
- * Returns the value of the last digit
+ * Return: the value of the last digit
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	
 	int lastDigit;
