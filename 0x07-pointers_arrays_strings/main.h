5b7cdf275074dfd9e7ff5934c566b53c52a744ff
mayor-of-geology <atolagbejoshua2@gmail.com>
2022-03-29 10:54:43 +0100

    [Update:] Added putchar and main.h file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define _main_h_
 
 char *_memset(char *s, char b, unsigned int n);
-char _putchar(char c);
+int _putchar(char);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
