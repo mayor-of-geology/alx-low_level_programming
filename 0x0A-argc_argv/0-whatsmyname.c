d65ab6d7259b70fc5c0b35db59f3baf3c1fe5432
mayor-of-geology <atolagbejoshua2@gmail.com>
2022-04-04 16:11:09 +0100

    [Update:]Adjusted script for compilation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argc: number of command line arguments
  * @argv: array of command line arguments
  */
-int main(int __attribute__((unused)) argc, char *argv)
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
